e5829f9f8bc429c3f6372f5b2ac854fe69fecb47
NIKHIL P L <nikhil.pl.email@gmail.com>
2019-04-01 21:52:06 +0530

    Initial commit

---
+++ src/main/java/com/demo/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping(value="home")
+
+public class HomeController {
+
+	@RequestMapping(method=RequestMethod.GET)	
+	public String index() {
+		return "home/index";
+	}
+}
